185173626182fcb26abd5b85964662351339c7b3
Marlon G <35308625+uhmarlon@users.noreply.github.com>
2023-05-12 10:32:23 +0200

    center this image

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -22,31 +22,30 @@ export const Home: NextPage = () => {
           <h1 className='text-4xl font-bold text-center mb-2'>Guess The Thing</h1>
           <span className='text-1xl text-center mb-2'>Challenge your friends or test your skills solo.</span>
           
-          <div className="flex flex-row justify-center mt-6">
 
-            <div className="relative w-1/2 h-[15rem] mb-3">
-              <Link href="/gussthecocktail">
-              <Image src="https://www.thecocktaildb.com/images/media/drink/nkwr4c1606770558.jpg" alt='' fill className="rounded-xl layout-fill object-cover" />
-              <div className="absolute bottom-0 w-full h-full bg-gradient-to-t from-black/80 to-transparent rounded-xl"></div>
-              <div className="absolute bottom-0 w-full h-full flex flex-col justify-end px-6 pb-6">
-                <h3 className="text-2xl font-bold mb-2 text-white">Guess The Cocktail</h3>
-                <p className="text-white/80">-</p>
-              </div>
-              </Link>
-            </div>
+          <div className="grid grid-cols-1 gap-1 m-4 justify-center items-center content-center justify-items-center">
+  <div className="relative w-1/2 h-[15rem] mb-3">
+    <Link href="/gussthecocktail">
+      <Image src="https://www.thecocktaildb.com/images/media/drink/nkwr4c1606770558.jpg" alt='' fill className="rounded-xl layout-fill object-cover" />
+      <div className="absolute bottom-0 w-full h-full bg-gradient-to-t from-black/80 to-transparent rounded-xl"></div>
+      <div className="absolute bottom-0 w-full h-full flex flex-col justify-end px-6 pb-6">
+        <h3 className="text-2xl font-bold mb-2 text-white">Guess The Cocktail</h3>
+        <p className="text-white/80">Guess the Cocktail of a Image</p>
+      </div>
+    </Link>
+  </div>
 
-            <div className="relative w-1/2 h-[15rem] mb-3">
-              <Link href="/gusstheflag">
-              <Image src="/flag.webp" alt='' fill className="rounded-xl layout-fill object-cover" />
-              <div className="absolute bottom-0 w-full h-full bg-gradient-to-t from-black/80 to-transparent rounded-xl"></div>
-              <div className="absolute bottom-0 w-full h-full flex flex-col justify-end px-6 pb-6">
-                <h3 className="text-2xl font-bold mb-2 text-white">Guess The Flag</h3>
-                <p className="text-white/80">Guess the flag of a country.</p>
-              </div>
-              </Link>
-            </div>
-
-          </div>
+  <div className="relative w-1/2 h-[15rem] mb-3">
+    <Link href="/gusstheflag">
+      <Image src="/flag.webp" alt='' fill className="rounded-xl layout-fill object-cover" />
+      <div className="absolute bottom-0 w-full h-full bg-gradient-to-t from-black/80 to-transparent rounded-xl"></div>
+      <div className="absolute bottom-0 w-full h-full flex flex-col justify-end px-6 pb-6">
+        <h3 className="text-2xl font-bold mb-2 text-white">Guess The Flag</h3>
+        <p className="text-white/80">Guess the flag of a country.</p>
+      </div>
+    </Link>
+  </div>
+</div>
         </div>
       </main>
     </>
