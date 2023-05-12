acc4dfc7c1a19f33e15c00326ed63322b357665c
Marlon G <35308625+uhmarlon@users.noreply.github.com>
2023-05-12 07:35:15 +0200

    Update index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -25,23 +25,23 @@ export const Home: NextPage = () => {
           <div className="flex flex-row justify-center mt-6">
 
             <div className="relative w-1/2 h-[15rem] mb-3">
-              <Link href="/gusstheflag">
-              <Image src="/flag.webp" alt='' fill className="rounded-xl layout-fill object-cover" />
+              <Link href="/gussthecocktail">
+              <Image src="https://www.thecocktaildb.com/images/media/drink/nkwr4c1606770558.jpg" alt='' fill className="rounded-xl layout-fill object-cover" />
               <div className="absolute bottom-0 w-full h-full bg-gradient-to-t from-black/80 to-transparent rounded-xl"></div>
               <div className="absolute bottom-0 w-full h-full flex flex-col justify-end px-6 pb-6">
-                <h3 className="text-2xl font-bold mb-2 text-white">Guess The Flag</h3>
-                <p className="text-white/80">Guess the flag of a country.</p>
+                <h3 className="text-2xl font-bold mb-2 text-white">Guess The Cocktail</h3>
+                <p className="text-white/80">-</p>
               </div>
               </Link>
             </div>
 
             <div className="relative w-1/2 h-[15rem] mb-3">
               <Link href="/gusstheflag">
-              <Image src="https://www.thecocktaildb.com/images/media/drink/nkwr4c1606770558.jpg" alt='' fill className="rounded-xl layout-fill object-cover" />
+              <Image src="/flag.webp" alt='' fill className="rounded-xl layout-fill object-cover" />
               <div className="absolute bottom-0 w-full h-full bg-gradient-to-t from-black/80 to-transparent rounded-xl"></div>
               <div className="absolute bottom-0 w-full h-full flex flex-col justify-end px-6 pb-6">
-                <h3 className="text-2xl font-bold mb-2 text-white">Guess The Cocktail</h3>
-                <p className="text-white/80">-</p>
+                <h3 className="text-2xl font-bold mb-2 text-white">Guess The Flag</h3>
+                <p className="text-white/80">Guess the flag of a country.</p>
               </div>
               </Link>
             </div>
