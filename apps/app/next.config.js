35ae6c82d9eec03e00b07bcc37f9782fd79dd5df
Turbobot <turbobot@vercel.com>
2024-04-28 20:19:09 +0200

    feat(create-turbo): apply official-starter transform

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["@repo/ui"],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  images: {
+    domains: ["www.thecocktaildb.com"],
+    unoptimized: true,
+  },
+  reactStrictMode: true,
 };
