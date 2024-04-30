f9577f6431cb39b61b4569563dbe47d0b419dbf3
Marlon G. <35308625+uhmarlon@users.noreply.github.com>
2024-04-30 13:08:22 +0200

    fix: header and page name

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -18,9 +18,11 @@ function Header(): JSX.Element {
             width={50}
           />
         </Link>
-        <nav className="hidden sm:flex">
-          <div className="font-bold">Guess The Thing</div>
-        </nav>
+        <Link href="/">
+          <nav className="hidden sm:flex">
+            <div className="font-bold">Guess The Thing</div>
+          </nav>
+        </Link>
 
         <nav className="hidden sm:flex gap-4 ml-6">
           <Link className="hover:text-gray-400" href="/home">
@@ -33,45 +35,44 @@ function Header(): JSX.Element {
       </div>
 
       <div className="flex items-center gap-2 mr-1 sm:mr-10">
-        <span className="rounded-full border border-white/20 bg-white/10 px-4 py-2 text-center font-medium text-white backdrop-blur-sm">
-          JOIN WITH CODE
-        </span>
+        <Link href="/join">
+          <span className="rounded-full border border-white/20 bg-white/10 px-4 py-2 text-center font-medium text-white backdrop-blur-sm">
+            JOIN WITH CODE
+          </span>
+        </Link>
         {session?.user ? (
-          <div>
-            <Link href="/me">
-              <div className="hidden sm:flex">
-                <React.Fragment>
-                  <span className="rounded-full border border-white/20 bg-white/10 px-4 py-1 text-center font-medium text-white backdrop-blur-sm whitespace-nowrap flex items-center">
-                    <Image
-                      alt="logo"
-                      className="h-2 w-2 sm:h-8 sm:w-8 rounded-full mr-2"
-                      height={50}
-                      src={session.user.image ?? ""}
-                      width={50}
-                    />
-                    {session.user.name?.toUpperCase()}
-                  </span>
-                </React.Fragment>
-              </div>
-              <div className="sm:hidden">
-                <Image
-                  alt="logo"
-                  className="h-9 w-9 rounded-full"
-                  height={50}
-                  src={session.user.image ?? ""}
-                  width={50}
-                />
-              </div>
-            </Link>
-          </div>
+          <Link href="/me">
+            <div className="hidden sm:flex">
+              <React.Fragment>
+                <span className="rounded-full border border-white/20 bg-white/10 px-4 py-1 text-center font-medium text-white backdrop-blur-sm whitespace-nowrap flex items-center">
+                  <Image
+                    alt="logo"
+                    className="h-2 w-2 sm:h-8 sm:w-8 rounded-full mr-2"
+                    height={50}
+                    src={session.user.image ?? ""}
+                    width={50}
+                  />
+                  {session.user.name?.toUpperCase()}
+                </span>
+              </React.Fragment>
+            </div>
+            <div className="sm:hidden">
+              <Image
+                alt="logo"
+                className="h-9 w-9 rounded-full"
+                height={50}
+                src={session.user.image ?? ""}
+                width={50}
+              />
+            </div>
+          </Link>
         ) : (
-          <button
-            className="mx-auto rounded-full border border-blue-500/90 bg-blue-500/40 px-4 py-2 text-center font-medium text-white backdrop-blur-sm"
+          <span
+            className="rounded-full border border-blue-500/90 bg-blue-500/40 px-4 py-2 text-center font-medium text-white backdrop-blur-sm"
             onClick={(): unknown => signIn()}
-            type="button"
           >
             LOGIN
-          </button>
+          </span>
         )}
       </div>
     </header>
