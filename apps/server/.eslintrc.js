2b2e089b460cad4cc39fa0fa50ccb536ea15f18b
Marlon G. <35308625+uhmarlon@users.noreply.github.com>
2024-05-01 12:16:07 +0200

    fix: lint error

--- apps/server/.eslintrc.js
+++ apps/server/.eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
-  extends: ["@repo/eslint-config/library.js"],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+      },
+    ],
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": ["error"],
+  },
+  ignorePatterns: [".eslintrc.js", "node_modules/"],
 };
--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,3 +1,39 @@
+// First, require the Next.js ESLint plugin
+const nextPlugin = require("@next/eslint-plugin-next");
+
 module.exports = {
-  extends: ["@repo/eslint-config/next.js"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@next/next/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  ignorePatterns: [".eslintrc.js", "node_modules/"],
+  plugins: ["react", "@next/next", "react-hooks", "@typescript-eslint"],
+  rules: {
+    ...nextPlugin.configs.recommended.rules, // Spread the recommended rules from the Next.js plugin
+    ...nextPlugin.configs["core-web-vitals"].rules, // Spread the core-web-vitals rules from the Next.js plugin
+    "max-statements-per-line": ["error", { max: 2 }],
+    "react/react-in-jsx-scope": "off", // Next.js does not require React to be in scope
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+  },
+  settings: {
+    react: {
+      version: "detect", // Automatically detect the React version
+    },
+  },
 };
