0f94d64591f431fcbf2193454f22e498e23c490a
Marlon G. <35308625+uhmarlon@users.noreply.github.com>
2024-05-12 16:55:12 +0200

    fix: update file name

--- apps/server/src/socket/index.ts
+++ apps/server/src/socket/index.ts
@@ -1,5 +1,5 @@
 import { Server } from "socket.io";
-import JoinHandler from "./joinHandler";
+import JoinHandler from "./joinhandler";
 import { AnswerHandler } from "./AnswerHandler";
 
 export class SocketSetup {
