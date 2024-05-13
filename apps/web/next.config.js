673b2c65b5bd4738713e6ef8780d938a970c7b6b
Marlon G. <35308625+uhmarlon@users.noreply.github.com>
2024-05-13 16:31:56 +0200

    fix: test

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,22 +5,33 @@ const withPWA = require("next-pwa")({
   skipWaiting: true, // Skip waiting for service worker activation
 });
 
+const path = require("path");
+
 const nextConfig = {
-  jsconfigPaths: true,
   reactStrictMode: true, // Enable React strict mode for improved error handling
   swcMinify: true, // Enable SWC minification for improved performance
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
-};
-
-const additionalConfig = {
-  webpack(config) {
+  webpack: (config, { isServer, buildId, dev, webpack }) => {
+    // Handle SVG files
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"], // Use SVGR for handling SVG files
+      use: ["@svgr/webpack"],
     });
+
+    // Resolve aliases based on tsconfig paths
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      src: path.resolve(__dirname, "src"),
+      public: path.resolve(__dirname, "public"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@api": path.resolve(__dirname, "src/api"),
+    };
+
     return config;
   },
   images: {
@@ -29,9 +40,4 @@ const additionalConfig = {
   },
 };
 
-const finalConfig = {
-  ...nextConfig,
-  ...additionalConfig,
-};
-
-module.exports = withPWA(finalConfig);
+module.exports = withPWA(nextConfig);
