80c4329c97b1f71a2f36f0364221336df5a75b47
Marlon G. <35308625+uhmarlon@users.noreply.github.com>
2024-06-06 20:17:17 +0200

    fix: connection

--- apps/web/src/utils/db/schema.ts
+++ apps/web/src/utils/db/schema.ts
@@ -9,7 +9,7 @@ import mysql from "mysql2/promise";
 import { drizzle } from "drizzle-orm/mysql2";
 import type { AdapterAccount } from "next-auth/adapters";
 
-export const connection = await mysql.createConnection({
+export const connection = await mysql.createPool({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || "3306"),
   user: process.env.DB_USER,
