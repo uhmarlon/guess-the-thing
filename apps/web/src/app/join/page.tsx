ab153d71e9aea4c53f20156ad263706481007048
Marlon G. <35308625+uhmarlon@users.noreply.github.com>
2024-06-07 13:20:58 +0200

    fix: useSearchParams suspense boundary

--- apps/web/src/app/join/page.tsx
+++ apps/web/src/app/join/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, Suspense } from "react";
 import { motion } from "framer-motion";
 import { Viewc } from "src/components/viewc";
 import { useRouter, useSearchParams } from "next/navigation";
 import { getBackendURL } from "../../utils/game-api";
 
-export default function Page(): JSX.Element {
+// Composant séparé pour le contenu principal
+function Content() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [code, setCode] = useState(new Array(4).fill(""));
@@ -89,39 +90,46 @@ export default function Page(): JSX.Element {
       }
     }
     firstInputRef.current?.focus();
-  }, [searchParams]);
+  }, [fetchGameToken, searchParams]);
 
+  return (
+    <div className="text-center text-white text-sm md:text-lg">
+      <div className="space-x-2 md:space-x-4 p-4 rounded-lg pt-2">
+        {code.map((digit, index) => (
+          <motion.input
+            key={index}
+            ref={inputRefs[index]}
+            className="w-14 h-14 md:w-32 md:h-32 text-center text-2xl md:text-6xl border-4 border-gttlightpurple bg-gttpurple rounded-lg"
+            type="text"
+            maxLength={1}
+            value={digit}
+            onChange={(e) => handleInput(e.target.value, index)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+            onPaste={(e) => handleInput(e.clipboardData.getData("Text"), index)}
+            transition={{ duration: 0.2 }}
+          />
+        ))}
+      </div>
+      {gameToken && (
+        <p className="text-center text-lg text-white">
+          Game Token: {gameToken}
+        </p>
+      )}
+    </div>
+  );
+}
+
+// Composant principal
+export default function Page(): JSX.Element {
   return (
     <Viewc>
       <main className="justify-center">
         <h1 className="text-1xl md:text-2xl text-center font-bold text-white pt-[32vh]">
           Enter the code
         </h1>
-        <div className="text-center text-white text-sm md:text-lg">
-          <div className="space-x-2 md:space-x-4 p-4 rounded-lg pt-2">
-            {code.map((digit, index) => (
-              <motion.input
-                key={index}
-                ref={inputRefs[index]}
-                className="w-14 h-14 md:w-32 md:h-32 text-center text-2xl md:text-6xl border-4 border-gttlightpurple bg-gttpurple rounded-lg"
-                type="text"
-                maxLength={1}
-                value={digit}
-                onChange={(e) => handleInput(e.target.value, index)}
-                onKeyDown={(e) => handleKeyDown(e, index)}
-                onPaste={(e) =>
-                  handleInput(e.clipboardData.getData("Text"), index)
-                }
-                transition={{ duration: 0.2 }}
-              />
-            ))}
-          </div>
-          {gameToken && (
-            <p className="text-center text-lg text-white">
-              Game Token: {gameToken}
-            </p>
-          )}
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Content />
+        </Suspense>
       </main>
     </Viewc>
   );
